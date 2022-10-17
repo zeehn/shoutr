fd8d1e8984f67c1a053dea07cb11e9114c16d291
Zakir Hussain <zack.mesacc@gmail.com>
2022-10-17 15:26:55 -0500

    Make shouts polymorphic

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -14,7 +14,15 @@ class ShoutsController < ApplicationController
 
   private
   def shout_params
-    params.require(:shout).permit(:body)
+    { content: content_from_params }
+  end
+
+  def content_from_params
+    TextShout.new(content_params)
+  end
+    
+  def content_params
+    params.require(:shout).require(:content).permit(:body)
   end
   
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_020434) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_061532) do
   create_table "shouts", force: :cascade do |t|
-    t.string "body", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content_type"
+    t.integer "content_id"
+    t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
     t.index ["user_id"], name: "index_shouts_on_user_id"
+  end
+
+  create_table "text_shouts", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
